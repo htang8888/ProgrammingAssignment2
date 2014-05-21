53c908f250fc3f06d403548bd8470caca7728351
Haofeng Tang <htang2000@hotmail.com>
2014-05-21 00:22:50 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions together compute the invert of an invertible square
+## matrix. Once the invert is calc'ed, the result is cached. So when the
+## invert is needed at any later time, no computation is needed.
 
-## Write a short comment describing this function
+## Function makeCacheMatrix(x) creates an object with the caching capability
+## of invert of matrix, together with accesser functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+			x <<- y
+			m <<- NULL
+        }
+	get <- function() x
+	setinvert <- function(invert) m <<- invert
+	getinvert <- function() m
+	list(set = set, get = get,
+		setinvert = setinvert,
+		getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve(x) checks into object x, see if its the invert is ready.
+## It is returned as result if so, otherwise, the actual computation occurs and
+## cahced, then result is returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+	m <- x$getinvert()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinvert(m)
+	m
 }
